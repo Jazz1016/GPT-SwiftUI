edfc6d3747d3cd6495caf1afcd0cdf580fafa1a4
James Lea <james.q.lea@gmail.com>
2023-05-16 14:49:25 -0600

    test

---
+++ GPT-SwiftUI/GPT_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPT_SwiftUIApp.swift
+//  GPT-SwiftUI
+//
+//  Created by James Lea on 5/16/23.
+//
+
+import SwiftUI
+
+@main
+struct GPT_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
